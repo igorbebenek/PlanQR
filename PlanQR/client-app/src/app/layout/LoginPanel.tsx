15fa048f04738eea5b6db3673ea3321cf4e44f31
Illia <karma@MYPC>
2024-12-03 21:21:17 +0100

    Log page

--- PlanQR/client-app/src/app/layout/LoginPanel.tsx
+++ PlanQR/client-app/src/app/layout/LoginPanel.tsx
@@ -1,54 +1,23 @@
-export default function LoginPanel() 
-{
+import "../layout/style.css"; 
+import logo from "../../assets/ZUT_Logo.png";
 
+export default function LoginPanel() {
   return (
-    <div style={styles.container}>
-      <input
-        type="email"
-        placeholder="Email"
-        style={styles.input}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        style={styles.input}
-      />
-      <button style={styles.button}>
-        Zaloguj
-      </button>
+    <div className="login-container">
+      <form className="login-form">
+        <div className="login-image">
+          <img src={logo} alt="Login" />
+        </div>
+        <div className="input-group-wrapper">
+          <div className="input-group">
+            <input type="email" placeholder="Email" required />
+          </div>
+          <div className="input-group">
+            <input type="password" placeholder="Password" required />
+          </div>
+        </div>
+        <button type="submit" className="login-btn">Zaloguj</button>
+      </form>
     </div>
   );
-};
-
-const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-    padding: "20px",
-  },
-  title: {
-    fontSize: "24px",
-    color: "#fff",
-    marginBottom: "20px",
-  },
-  input: {
-    width: "300px",
-    padding: "10px",
-    margin: "10px 0",
-    border: "1px solid #ccc",
-    borderRadius: "5px",
-  },
-  button: {
-    width: "300px",
-    padding: "10px",
-    backgroundColor: "#00C344",
-    border: "none",
-    color: "#fff",
-    fontSize: "16px",
-    borderRadius: "5px",
-    cursor: "pointer",
-  },
 };
