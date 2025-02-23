0ca96cb0b1d91335310e3cfeb549633c7c9f8ab8
ms53692 <ms53692@zut.edu.pl>
2025-02-23 15:43:56 +0100

    Changes to seed.cs

--- PlanQR/Persistence/Seed.cs
+++ PlanQR/Persistence/Seed.cs
@@ -9,48 +9,12 @@ public class Seed{
         if (context.Messages.Any())
         {
             Console.WriteLine("Chats table already has data.");
-            context.Database.ExecuteSqlRaw("DELETE FROM Messages");
         }
         else
         {
             Console.WriteLine("Messages table is empty or does not exist.");
         }
 
-        var messages = new[]
-        {
-            new Message
-            {
-                createdAt = DateTime.UtcNow,
-                body = "Hello",
-                lecturer = "Kowalski",
-                login = "kowal",
-                room = "101",
-                lessonId = 1,
-                group = "1A"
-            },
-            new Message
-            {
-                createdAt = DateTime.UtcNow,
-                body = "Hi",
-                lecturer = "Nowak",
-                login = "nowak",
-                room = "102",
-                lessonId = 2,
-                group = "1B"
-            },
-            new Message
-            {
-                createdAt = DateTime.UtcNow,
-                body = "Hey",
-                lecturer = "Kowal",
-                login = "kowal",
-                room = "103",
-                lessonId = 3,
-                group = "1C"
-            }
-        };
-
-        await context.Messages.AddRangeAsync(messages);
         await context.SaveChangesAsync();
     }
 }
