8c483d2f9080e1924901bb605fa4b293d7b5d501
Igor Bębenek <139233781+igorbebenek@users.noreply.github.com>
2025-02-26 21:27:07 +0100

    commit

---
+++ PlanQR/client-app/src/app/layout/Tablet.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../layout/NavBar";
+import './Tablet.css';
+
+
+export default function Tablet() 
+{
+    return(
+        <>
+           <NavBar />
+           <Tablet />
+        </>
+    )
+}
--- PlanQR/client-app/src/app/router/Routes.tsx
+++ PlanQR/client-app/src/app/router/Routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouteObject } from "react-router-dom";
 import App from "../../App";
 import LecturerPlan from "../features/LecturerPlan";
 import PlanDetails from "../features/PlanDetails";
+import Tablet from "../layout/Tablet";
+
 
 export const routes: RouteObject[] = [
     {
@@ -10,6 +12,7 @@ export const routes: RouteObject[] = [
         children: [
             {path: 'LecturerPlan/:teacher',element: <LecturerPlan />},
             {path: ':department/:room',element: <PlanDetails />},
+            {path: 'tablet/:department/:room',element: <Tablet />},
         ]
     }
 ]
