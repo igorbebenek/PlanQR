1d01c11b131bba72c71e04097ba9a0d65f2de140
lukaszwatral <lukaszwatral@gmail.com>
2025-06-10 12:06:28 +0200

    Konfiguracja serwera vite

--- PlanQR/client-app/vite.config.ts
+++ PlanQR/client-app/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="node" />
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import fs from 'fs'
 import path, { dirname } from 'path'
@@ -8,33 +8,38 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname,'../certs/planqr.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../certs/qrplan.pem')),
-    },
-    port: 443,
-    host: true,
-    proxy: {
-      '/schedule_student.php': {
-        target: 'https://plan.zut.edu.pl',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/schedule_student.php/, '/schedule_student.php')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const siteUrl = env.VITE_SITE_URL // Np. https://planqr.wi.zut.edu.pl
+
+  return {
+    server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname,'../certs/planqr.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, '../certs/qrplan.pem')),
       },
-      '/api': {
-        target: 'https://192.168.203.248', // Twój backend
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Usuwa prefix `/api` (jeśli nie jest potrzebny)
+      port: 443,
+      host: true,
+      proxy: {
+        '/schedule_student.php': {
+          target: 'https://plan.zut.edu.pl', // Możesz dodać też to do .env, jeśli chcesz
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/schedule_student.php/, '/schedule_student.php'),
+        },
+        '/api': {
+          target: siteUrl,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+      hmr: {
+        host: new URL(siteUrl).hostname, // Używa samej domeny np. planqr.wi.zut.edu.pl
+        protocol: 'wss',
       },
     },
-    hmr: {
-      host: 'planqr.wi.zut.edu.pl', // Ustaw hosta na domenę aplikacji
-      protocol: 'wss', // Użyj WebSocket Secure (wss) dla HTTPS
-    },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+  }
+})
